6559b67db355f70463b455eb58441e7c113bdf3c
Neisvestney <neisvestney113@gmail.com>
2020-10-17 15:32:10 +0300

    Damage cube together!

---
+++ Assets/Resources/Game/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public GameObject batteryPrefab;
+    public GameObject batteryTarget;
+
+    public List<Player> players = new List<Player>();
+
+    public Battery battery;
+
+    void Start()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        if (NetworkManager.singleton.isNetworkActive)
+        {
+            foreach (KeyValuePair<uint, NetworkIdentity> kvp in NetworkIdentity.spawned)
+            {
+                Player comp = kvp.Value.GetComponent<Player>();
+                if (!players.Contains(comp)) players.Add(comp);
+            }
+        }
+        else
+        {
+            players.Clear();
+        }
+    }
+}
--- Assets/Resources/Game/Player/Player.cs
+++ Assets/Resources/Game/Player/Player.cs
@@ -7,11 +7,16 @@ using Vuforia;
 
 public class Player : NetworkBehaviour
 {
+    public GameObject UI;
+    public List<GameObject> hostOnlyObjects;
+
     private GameObject mainCamera;
 
     private void Start()
     {
         if (isLocalPlayer) mainCamera = Camera.main.gameObject;
+        else UI.SetActive(false);
+        if (!isServer) hostOnlyObjects.ForEach(o => o.SetActive(false));
     }
 
     private void Update()
@@ -20,5 +25,19 @@ public class Player : NetworkBehaviour
 
         transform.position = mainCamera.transform.position;
         transform.rotation = mainCamera.transform.rotation;
+    }
+
+    [Command]
+    public void CmdStartGame()
+    {
+        GameObject battery = Instantiate(GameManager.instance.batteryPrefab, Vector3.zero, Quaternion.identity,
+            GameManager.instance.batteryTarget.transform);
+        NetworkServer.Spawn(battery);
+    }
+
+    [Command]
+    public void CmdDamage(int damage)
+    {
+        GameManager.instance.battery.health -= damage;
     }
 }
