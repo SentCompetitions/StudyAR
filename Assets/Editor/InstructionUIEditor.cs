0669e713ade39a47b98af11c683b2c9663cf7b00
Neisvestney <neisvestney113@gmail.com>
2020-10-31 12:47:00 +0300

    Fix editor script

---
+++ Assets/Editor/InstructionUIEditor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(InstructionUI)), CanEditMultipleObjects]
+public class InstructionUIEditor : Editor {
+         
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        serializedObject.ApplyModifiedProperties();
+    }
+}
