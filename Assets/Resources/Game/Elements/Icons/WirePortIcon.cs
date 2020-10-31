84ca817cd86ed89f1de018bb81f6ffe2d9e4656f
Xefise <Xefise@gmail.com>
2020-10-31 10:57:42 +0300

    Add disapiring icons

--- Assets/Resources/Game/Elements/Icons/WirePortIcon.cs
+++ Assets/Resources/Game/Elements/Icons/WirePortIcon.cs
@@ -14,10 +14,15 @@ public class WirePortIcon : MonoBehaviour
         transform.position = mainCamera.WorldToScreenPoint(target.position);
 
         if (!image || !player) return;
-        float resolution = 3f + (float)Math.Pow(0.75f * Screen.dpi, 1.12f);
+        float distance = Vector3.Distance(target.transform.position, player.transform.position);
+        if (distance > 0.5f)
+        {
+            image.rectTransform.sizeDelta = new Vector2(0, 0);//OPTIMIZATION Change the code to more optimized
+            return;
+        }
+        float resolution = 3f + (float)Math.Pow(0.21f * Screen.dpi, 1.05f);
         if (resolution == 0) resolution = 25f;
-        resolution = resolution / (0.1f + Vector3.Distance(target.transform.position, player.transform.position) * 7);
+        resolution = resolution / (0.1f + distance * 7);
         image.rectTransform.sizeDelta = new Vector2(resolution, resolution);
-
     }
 }
