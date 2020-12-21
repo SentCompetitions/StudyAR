eee69eb325c323bcda234ce5928ef9044b5dbf51
Neisvestney <neisvestney113@gmail.com>
2020-12-21 22:48:13 +0300

    Add CopyToPersistent and GetPacks methods

--- Assets/Resources/Menu/ExperienceManager.cs
+++ Assets/Resources/Menu/ExperienceManager.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,30 +8,64 @@ using UnityEngine.UI;
 public class ExperienceManager : MonoBehaviour
 {
     public Text path;
+
     void Start()
     {
-        // Coping packs from StreamingAssets to Application.persistentDataPath
+        CopyToPersistent();
+
+        path.text = "Загруженные сбоники: " + String.Join(", ", GetPacks().Select(x => x.name).ToArray())
+            + '\n' + Application.persistentDataPath;
+    }
+
+    /// <summary>
+    /// Copy packs from StreamingAssets to Application.persistentDataPath
+    /// </summary>
+    private void CopyToPersistent()
+    {
         BetterStreamingAssets.Initialize();
 
-        string[] paths = BetterStreamingAssets.GetFiles("Packs", "pack.json", SearchOption.AllDirectories);
-        for (int i = 0; i < paths.Length; i++)
-        {
-            paths[i] = Path.GetDirectoryName(paths[i]);
-        }
+        string[] paths = BetterStreamingAssets.GetFiles("Packs", "*", SearchOption.AllDirectories);
 
-        path.text = Application.persistentDataPath + '\n' + String.Join(", ", paths);
+        string dirPath = Path.Combine(Application.persistentDataPath, "Packs", "BuildIn");
+        if (Directory.Exists(dirPath))
+            Directory.Delete(dirPath, true);
+        Directory.CreateDirectory(dirPath);
 
-        foreach (string packPath in paths)
+        foreach (string filePath in paths)
         {
-            string dirPath = Path.Combine(Application.persistentDataPath, packPath);
-            if (Directory.Exists(dirPath))
-                Directory.Delete(dirPath, true);
-            Directory.CreateDirectory(dirPath);
-            foreach (string file in BetterStreamingAssets.GetFiles(packPath, "*", SearchOption.TopDirectoryOnly))
-            {
-                byte[] data = BetterStreamingAssets.ReadAllBytes(file);
-                File.WriteAllBytes(Path.Combine(Application.persistentDataPath, file), data);
-            }
+            byte[] data = BetterStreamingAssets.ReadAllBytes(filePath);
+            string file = Path.Combine(
+                Application.persistentDataPath, "Packs", "BuildIn",
+                Path.Combine(filePath.Split('/').Skip(1).ToArray())
+            );
+            Directory.CreateDirectory(Path.GetDirectoryName(file)!);
+            File.WriteAllBytes(file, data);
         }
     }
+
+    private List<Pack> GetPacks()
+    {
+        List<Pack> packs = new List<Pack>();
+
+        foreach (string packFile in  Directory.GetFiles(Path.Combine(Application.persistentDataPath, "Packs"), "pack.json", SearchOption.AllDirectories))
+        {
+            try
+            {
+                packs.Add(JsonUtility.FromJson<Pack>(File.ReadAllText(packFile)));
+                Debug.Log("[ExperienceManager] Loaded " + packFile);
+            }
+            catch (ArgumentException e)
+            {
+                Debug.LogError("[ExperienceManager] Error on loading " + packFile);
+            }
+        }
+
+        return packs;
+    }
+}
+
+[Serializable]
+public struct Pack
+{
+    public string name;
 }
